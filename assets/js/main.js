3466c6919854bfba003a26b581d4a6a58400426c
Ibrah-55 <kariukiibrahim55@gmail.com>
2022-06-07 12:46:14 +0300

    whatsapp chat

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,10 +81,7 @@
 
 	};
 
-	// Play initial animations on page load.
-	$(document).ready(function() {
-		$("#my_audio").get(0).play();
-	});
+	// Play initial animations on page load
 		$window.on('load', function() {
 			window.setTimeout(function() {
 				$body.removeClass('is-preload');
